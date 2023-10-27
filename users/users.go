7280ae52a6a3d13112fb388e782b90f249837828
LENDRA SYAPUTRA <l3noneone@gmail.com>
2023-10-28 02:35:48 +0700

    Membuat tabel user connect db

--- users/users.go
+++ users/users.go
@@ -1,22 +1,15 @@
+
 package users
 
 import (
-	"sqlgo/model"
-
-	"gorm.io/gorm"
+	"time"
 )
 
-type UsersSystem struct {
-	DB *gorm.DB
-}
-
-func (us *UsersSystem) ListUsers() ([]model.User, error) {
-	var result = make([]model.User, 0)
-	var qry = us.DB.Table("pelanggan").Find(&result)
-	var err = qry.Error
-	if err != nil {
-		return nil, err
-	}
-
-	return result, nil
+type User struct {
+	UserID    uint `gorm:"primaryKey"`
+	Username  string
+	Password  string
+	Role      string
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
