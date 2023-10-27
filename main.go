9d49bd4c672852528c33a403584cfdb350572331
LENDRA SYAPUTRA <l3noneone@gmail.com>
2023-10-28 03:00:53 +0700

    Membuat tabel customer, product, receipt, transaksi

--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"fmt"
 	"sqlgo/config"
+	"sqlgo/customers"
+	"sqlgo/products"
+	"sqlgo/receipts"
+	"sqlgo/transactions"
 	"sqlgo/users"
 )
 
@@ -13,7 +17,7 @@ func main() {
 		return
 	}
 
-	err = db.AutoMigrate(&users.User{})
+	err = db.AutoMigrate(&users.User{}, &customers.Customer{}, &products.Product{}, &receipts.Receipt{}, &transactions.Transaction{})
 	if err != nil {
 		fmt.Println("Ada Masalah", err.Error())
 		return
