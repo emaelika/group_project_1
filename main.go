17402af3eaad2328837f7eaff8805bd91f6ecfad
Abdul Malik <malik.the5th@gmail.com>
2023-10-28 09:25:10 +0700

    perubahan pada model dan main

--- main.go
+++ main.go
@@ -2,12 +2,9 @@ package main
 
 import (
 	"fmt"
+	"sqlgo/auth"
 	"sqlgo/config"
-	"sqlgo/customers"
-	"sqlgo/products"
-	"sqlgo/receipts"
-	"sqlgo/transactions"
-	"sqlgo/users"
+	"sqlgo/model"
 )
 
 func main() {
@@ -17,11 +14,37 @@ func main() {
 		return
 	}
 
-	err = db.AutoMigrate(&users.User{}, &customers.Customer{}, &products.Product{}, &receipts.Receipt{}, &transactions.Transaction{})
+	err = db.AutoMigrate(&model.User{}, &model.Customer{}, &model.Product{}, &model.Receipt{}, &model.Transaction{})
 	if err != nil {
 		fmt.Println("Ada Masalah", err.Error())
 		return
 	}
+	var auth = auth.AuthSystem{DB: db}
+	for {
+		var inputMenu int
+		fmt.Println("Selamat datang di Tokoku App!\nMenu:\n1. Log in\n0. Exit\nMasukkan input: ")
+		fmt.Scanln(&inputMenu)
+		switch inputMenu {
+		case 1:
+			var menuLogin int
+			result, permit := auth.Login()
+			if permit && result.Username == "admin" {
+				fmt.Println("\nSelamat datang ", result.Username)
+				for permit {
+					fmt.Println("\n1.")
+					fmt.Println("2. ")
+					fmt.Println("3. ")
+					fmt.Println("4. ")
+					fmt.Println("0. Logout")
+					fmt.Println("99. Exit")
+					fmt.Print("Masukkan pilihan: ")
+					fmt.Scanln(&menuLogin)
+				}
+			}
 
-	fmt.Println("Tabel berhasil dibuat")
+		case 0:
+			fmt.Println("\nTerima kasih!")
+			return
+		}
+	}
 }
