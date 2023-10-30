bcffb91c74add7766f2a9d50c1d02e87c115ed81
LENDRA SYAPUTRA <l3noneone@gmail.com>
2023-10-31 00:27:29 +0700

    update menu

--- main.go
+++ main.go
@@ -47,37 +47,4 @@ func main() {
 		}
 	}
 }
-
-
-
-
-
-
-
-
-
-		// fmt.Println("Selamat datang di Tokoku App!\nMenu:\n1. Log in\n0. Exit\nMasukkan input: ")
-		// fmt.Scanln(&inputMenu)
-		// switch inputMenu {
-		// case 1:
-		// 	var menuLogin int
-		// 	result, permit := auth.Login()
-		// 	if permit && result.Username == "admin" {
-		// 		fmt.Println("\nSelamat datang ", result.Username)
-		// 		for permit {
-		// 			fmt.Println("\n1.")
-		// 			fmt.Println("2. ")
-		// 			fmt.Println("3. ")
-		// 			fmt.Println("4. ")
-		// 			fmt.Println("0. Logout")
-		// 			fmt.Println("99. Exit")
-		// 			fmt.Print("Masukkan pilihan: ")
-		// 			fmt.Scanln(&menuLogin)
-		// 		}
-		// 	}
-
-		// case 0:
-		// 	fmt.Println("\nTerima kasih!")
-		// 	return
-		// }
-	}
+}
